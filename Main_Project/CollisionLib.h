43a5b0ec8f123ffca79b96008d350ee7c757f282
Mantas Zalnierius <C00241626@itcarlow.ie>
2022-03-24 13:26:36 +0000

    Added in a collision handler, using the objects. Still need to import SAT

---
+++ Main_Project/CollisionLib.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "GlobalHeaders.h"
+#include "Object.h"
+class CollisionHandler
+{
+public:
+	static inline void Circle2Circle(std::weak_ptr<Object> t_first, std::weak_ptr<Object> t_second)
+	{
+		if (t_first.lock()->getGlobalBounds().intersects(t_second.lock()->getGlobalBounds()))
+		{
+			t_first.lock()->setColor(sf::Color::Cyan);
+			t_second.lock()->setColor(sf::Color::Cyan);
+		}
+		else
+		{
+			t_first.lock()->setColor(sf::Color::Red);
+			t_second.lock()->setColor(sf::Color::Red);
+		}
+	}
+};
