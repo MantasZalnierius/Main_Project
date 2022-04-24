a8678d6471a7634e0e1dbd06e3a8fefded949dfe
Mantas Zalnierius <C00241626@itcarlow.ie>
2022-04-25 00:33:40 +0100

    Refactored Node class

--- Main_Project/Node.cpp
+++ Main_Project/Node.cpp
@@ -1,194 +1,210 @@
 #include "Node.h"
 
-Node::Node(sf::FloatRect bounds, size_t level,
-    size_t const& maxLevel, size_t const& maxObjects)
-    : m_border{ bounds }, m_depth{ level }, m_maxDepth{ maxLevel }, m_maxObjects{ maxObjects }
+Node::Node(sf::FloatRect t_shapeBounds, size_t t_level, size_t const& t_maxLevel, size_t const& t_maxObjects) :
+    m_shapeBounds{ t_shapeBounds },
+    m_depth{ t_level },
+    m_maxDepth{ t_maxLevel },
+    m_maxObjects{ t_maxObjects }
 {}
 
 size_t Node::countObjects() const
 {
     if (!m_canSubdivide)
-        return std::count_if(m_objects.begin(), m_objects.end(),
-            [](auto& obj)->bool { return !obj.expired(); });
-
-
-    size_t objCount = std::count_if(m_objects.begin(), m_objects.end(),
-        [](auto& obj)->bool { return !obj.expired(); });
-
-    for (auto const& child : m_childNodes)
+    {
+        return std::count_if(m_objects.begin(), m_objects.end(), [](auto& obj)->bool { return !obj.expired(); });
+    }
+    size_t objCount = std::count_if(m_objects.begin(), m_objects.end(), [](auto& obj)->bool { return !obj.expired(); });
+    for (auto& child : m_childNodes)
+    {
         objCount += child->countObjects();
-
+    }
     return objCount;
 }
 
 void Node::subdivide()
 {
     if (m_canSubdivide)
+    {
         return;
-
-    float midWidth{ m_border.width / 2.f }, midHeight{ m_border.height / 2.f };
-    float left{ m_border.left }, top{ m_border.top };
-
-    m_childNodes[0] = std::make_unique<Node>(sf::FloatRect{ left, top, midWidth, midHeight }, m_depth+ 1, m_maxDepth, m_maxObjects);
-    m_childNodes[1] = std::make_unique<Node>(sf::FloatRect{ left + midWidth, top,midWidth, midHeight }, m_depth + 1, m_maxDepth, m_maxObjects);
-    m_childNodes[2] = std::make_unique<Node>(sf::FloatRect{ left, top + midHeight, midWidth, midHeight }, m_depth + 1, m_maxDepth, m_maxObjects);
-    m_childNodes[3] = std::make_unique<Node>(sf::FloatRect{ left + midWidth, top + midHeight, midWidth, midHeight }, m_depth + 1, m_maxDepth, m_maxObjects);
-
-    for (auto& child : m_childNodes)
-        child->m_head = this;
-
-    m_canSubdivide = true;
+    }
+    else
+    {
+        float midWidth{ m_shapeBounds.width / 2.0f }, midHeight{ m_shapeBounds.height / 2.0f };
+        float left{ m_shapeBounds.left }, top{ m_shapeBounds.top };
+        m_childNodes[0] = std::make_unique<Node>(sf::FloatRect{ left, top, midWidth, midHeight }, m_depth + 1, m_maxDepth, m_maxObjects);
+        m_childNodes[1] = std::make_unique<Node>(sf::FloatRect{ left + midWidth, top,midWidth, midHeight }, m_depth + 1, m_maxDepth, m_maxObjects);
+        m_childNodes[2] = std::make_unique<Node>(sf::FloatRect{ left, top + midHeight, midWidth, midHeight }, m_depth + 1, m_maxDepth, m_maxObjects);
+        m_childNodes[3] = std::make_unique<Node>(sf::FloatRect{ left + midWidth, top + midHeight, midWidth, midHeight }, m_depth + 1, m_maxDepth, m_maxObjects);
+        for (auto& child : m_childNodes)
+        {
+            child->m_head = this;
+        }
+        m_canSubdivide = true;
+    }
 }
 
 void Node::insert(std::weak_ptr<Object> ptr)
 {
-    if (m_canSubdivide) {
-        // Try to insert it in a subnode
-        auto i{ getIndex(ptr.lock()->getGlobalBounds()) };
-
-        if (contains(m_childNodes[i]->m_border, ptr.lock()->getGlobalBounds())) {
-            m_childNodes[i]->insert(std::move(ptr));
+    if (m_canSubdivide)
+    {
+        size_t index = getIndex(ptr.lock()->getGlobalBounds());
+        if (contains(m_childNodes[index]->m_shapeBounds, ptr.lock()->getGlobalBounds()))
+        {
+            m_childNodes[index]->insert(ptr);
             return;
         }
-
-        // If it reaches this point, then it's between subnodes
-        m_collidedObjects.push_back(std::move(ptr));
+        m_colliders.push_back(ptr);
         return;
     }
-
-    m_objects.push_back(std::move(ptr));
-
+    m_objects.push_back(ptr);
     if (m_depth < m_maxDepth && m_objects.size() > m_maxObjects)
     {
         subdivide();
-
-        for (auto& obj : m_objects)
-            insert(std::move(obj));
-
+        for (auto& object : m_objects)
+        {
+            insert(object);
+        }
         m_objects.clear();
+    }
+}
+
+void Node::render(sf::RenderTarget& t_target) const
+{
+    sf::RectangleShape shape(sf::Vector2f(m_shapeBounds.width, m_shapeBounds.height));
+    shape.setPosition(m_shapeBounds.left, m_shapeBounds.top);
+    shape.setOutlineThickness(2.0f);
+    shape.setOutlineColor(sf::Color::Black);
+    shape.setFillColor(sf::Color::Transparent);
+    t_target.draw(shape);
+    if (m_canSubdivide)
+    {
+        for (auto& child : m_childNodes)
+        {
+            child->render(t_target);
+        }
     }
 }
 
 void Node::clear()
 {
-    m_collidedObjects.clear();
+    m_colliders.clear();
     m_objects.clear();
-
     if (!m_canSubdivide)
+    {
         return;
-
-    for (auto& child : m_childNodes) {
-        child->clear();
-        child.reset(nullptr);
     }
-
-    m_canSubdivide = false;
-}
-
-void Node::drawBorders(sf::RenderTarget& t) const
-{
-    sf::RectangleShape border{ sf::Vector2f{m_border.width, m_border.height} };
-    border.setPosition(m_border.left, m_border.top);
-    border.setOutlineThickness(2.0f);
-    border.setOutlineColor(sf::Color::White);
-    border.setFillColor(sf::Color::Transparent);
-    t.draw(border);
-
-    if (m_canSubdivide)
+    else
+    {
         for (auto& child : m_childNodes)
-            child->drawBorders(t);
+        {
+            child->clear();
+            child.reset(nullptr);
+        }
+        m_canSubdivide = false;
+    }
 }
 
-size_t Node::getIndex(sf::FloatRect objBounds) const
+size_t Node::getIndex(sf::FloatRect t_objectBounds) const
 {
-    float midWidth{ m_border.left + (m_border.width / 2.f) },
-        midHeight{ m_border.top + (m_border.height / 2.f) };
-
-    bool left{ objBounds.left <= midWidth },
-        top{ objBounds.top <= midHeight };
-
-
-    size_t index = NW;
-    if (left) {
+    float midWidth = m_shapeBounds.left + (m_shapeBounds.width / 2.0f);
+    float midHeight = m_shapeBounds.top + (m_shapeBounds.height / 2.0f);
+    bool left = t_objectBounds.left <= midWidth;
+    bool top = t_objectBounds.top <= midHeight;
+    size_t index = North;
+    if (left)
+    {
         if (!top)
-            index = SW;
-    }
-    else {
-        if (top) {
-            index = NE;
-        }
-        else {
-            index = SE;
+        {
+            index = WEST;
         }
     }
-
+    else
+    {
+        if (top)
+        {
+            index = EAST;
+        }
+        else
+        {
+            index = SOUTH;
+        }
+    }
     return index;
 }
 
-std::vector<std::weak_ptr<Object>> Node::getCollisionable(sf::FloatRect const& bounds) const
+std::vector<std::weak_ptr<Object>> Node::getColliders(sf::FloatRect const& t_objectBounds) const
 {
-    if (!contains(m_border, bounds))
+    if (!contains(m_shapeBounds, t_objectBounds))
+    {
         return {};
-
-    std::vector<std::weak_ptr<Object>> ret;
-    if (m_canSubdivide) {
-        size_t possibleIndex{ getIndex(bounds) };
-        if (contains(m_childNodes[possibleIndex]->m_border, bounds)) {
-            auto tmp{ m_childNodes[possibleIndex]->getCollisionable(bounds) };
-            ret.insert(ret.end(), std::make_move_iterator(tmp.begin()), std::make_move_iterator(tmp.end()));
-        }
-        else {
-            if (bounds.left <= m_childNodes[NE]->m_border.left) {
-                if (bounds.top < m_childNodes[SW]->m_border.top) {
-                    auto tmp{ m_childNodes[NW]->getAllObjects() };
-                    ret.insert(ret.end(), std::make_move_iterator(tmp.begin()), std::make_move_iterator(tmp.end()));
-                }
-
-                if (bounds.top + bounds.height > m_childNodes[SW]->m_border.top) {
-                    auto tmp{ m_childNodes[SW]->getAllObjects() };
-                    ret.insert(ret.end(), std::make_move_iterator(tmp.begin()), std::make_move_iterator(tmp.end()));
-                }
-            }
-
-            if (bounds.left + bounds.width > m_childNodes[NE]->m_border.left) {
-                if (bounds.top <= m_childNodes[SE]->m_border.top) {
-                    auto tmp{ m_childNodes[NE]->getAllObjects() };
-                    ret.insert(ret.end(), std::make_move_iterator(tmp.begin()), std::make_move_iterator(tmp.end()));
-                }
-
-                if (bounds.top + bounds.height > m_childNodes[SE]->m_border.top) {
-                    auto tmp{ m_childNodes[SE]->getAllObjects() };
-                    ret.insert(ret.end(), std::make_move_iterator(tmp.begin()), std::make_move_iterator(tmp.end()));
-                }
-            }
-        }
     }
+    else
+    {
+        std::vector<std::weak_ptr<Object>> ret;
+        if (m_canSubdivide)
+        {
+            size_t possibleIndex = getIndex(t_objectBounds);
+            if (contains(m_childNodes[possibleIndex]->m_shapeBounds, t_objectBounds))
+            {
+                auto temp = m_childNodes[possibleIndex]->getColliders(t_objectBounds);
+                ret.insert(ret.end(), std::make_move_iterator(temp.begin()), std::make_move_iterator(temp.end()));
+            }
+            else
+            {
+                if (t_objectBounds.left <= m_childNodes[EAST]->m_shapeBounds.left)
+                {
+                    if (t_objectBounds.top < m_childNodes[WEST]->m_shapeBounds.top)
+                    {
+                        auto temp = m_childNodes[North]->getObjects();
+                        ret.insert(ret.end(), std::make_move_iterator(temp.begin()), std::make_move_iterator(temp.end()));
+                    }
+                    if (t_objectBounds.top + t_objectBounds.height > m_childNodes[WEST]->m_shapeBounds.top)
+                    {
+                        auto temp = m_childNodes[WEST]->getObjects();
+                        ret.insert(ret.end(), std::make_move_iterator(temp.begin()), std::make_move_iterator(temp.end()));
+                    }
+                }
 
-    ret.insert(ret.end(), m_collidedObjects.begin(), m_collidedObjects.end());
-    ret.insert(ret.end(), m_objects.begin(), m_objects.end());
-
-    return std::move(ret);
+                if (t_objectBounds.left + t_objectBounds.width > m_childNodes[EAST]->m_shapeBounds.left)
+                {
+                    if (t_objectBounds.top <= m_childNodes[SOUTH]->m_shapeBounds.top)
+                    {
+                        auto temp = m_childNodes[EAST]->getObjects();
+                        ret.insert(ret.end(), std::make_move_iterator(temp.begin()), std::make_move_iterator(temp.end()));
+                    }
+                    if (t_objectBounds.top + t_objectBounds.height > m_childNodes[SOUTH]->m_shapeBounds.top)
+                    {
+                        auto tmp{ m_childNodes[SOUTH]->getObjects() };
+                        ret.insert(ret.end(), std::make_move_iterator(tmp.begin()), std::make_move_iterator(tmp.end()));
+                    }
+                }
+            }
+        }
+        ret.insert(ret.end(), m_colliders.begin(), m_colliders.end());
+        ret.insert(ret.end(), m_objects.begin(), m_objects.end());
+        return ret;
+    }
 }
 
-std::vector<std::weak_ptr<Object>> Node::getAllObjects() const
+std::vector<std::weak_ptr<Object>> Node::getObjects() const
 {
     std::vector<std::weak_ptr<Object>> ret;
-
     if (m_canSubdivide)
-        for (auto const& child : m_childNodes) {
-            auto tmp{ child->getAllObjects() };
-            ret.insert(ret.end(), tmp.begin(), tmp.end());
+    {
+        for (auto const& child : m_childNodes)
+        {
+            auto temp = child->getObjects();
+            ret.insert(ret.end(), temp.begin(), temp.end());
         }
-
+    }
     ret.insert(ret.end(), m_objects.begin(), m_objects.end());
-    ret.insert(ret.end(), m_collidedObjects.begin(), m_collidedObjects.end());
-
+    ret.insert(ret.end(), m_colliders.begin(), m_colliders.end());
     return ret;
 }
 
-bool Node::contains(sf::FloatRect const& first, sf::FloatRect const& second)
+bool Node::contains(sf::FloatRect const& t_firstObject, sf::FloatRect const& t_secondObject)
 {
-    return (first.left <= second.left && first.top <= second.top
-        && first.left + first.width >= second.left + second.width
-        && first.top + first.height >= second.top + second.height);
+    return (t_firstObject.left <= t_secondObject.left && t_firstObject.top <= t_secondObject.top
+        && t_firstObject.left + t_firstObject.width >= t_secondObject.left + t_secondObject.width
+        && t_firstObject.top + t_firstObject.height >= t_secondObject.top + t_secondObject.height);
 }
--- Main_Project/Node.h
+++ Main_Project/Node.h
@@ -2,57 +2,39 @@
 #include "GlobalHeaders.h"
 #include "Object.h"
 
-class QuadTree;
-
-enum Position 
-{ 
-    NW = 0,
-    NE = 1, 
-    SW = 2, 
-    SE = 3 
+enum Cardinal
+{
+    North = 0,
+    EAST = 1,
+    WEST = 2,
+    SOUTH = 3
 };
 
+class QuadTree;
 
 class Node
 {
     friend class QuadTree;
 public:
-    Node(sf::FloatRect bounds, size_t level,
-        size_t const& maxLevel, size_t const& maxObjects);
-
+    Node(sf::FloatRect t_shapeBounds, size_t t_level, size_t const& t_maxLevel, size_t const& t_maxObjects);
     size_t countObjects() const;
-
-    static bool contains(sf::FloatRect const& first, sf::FloatRect const& second);
-
+    static bool contains(sf::FloatRect const& t_firstObject, sf::FloatRect const& t_secondObject);
+    void render(sf::RenderTarget& t_target) const;
 private:
-    /// These member functions should be called by this or the QuadTree class!
-
     void insert(std::weak_ptr<Object> object);
-
-    void drawBorders(sf::RenderTarget& t) const;
-
     void subdivide();
-
     void clear();
-
-    size_t getIndex(sf::FloatRect objBounds) const;
-
-    std::vector<std::weak_ptr<Object>> getCollisionable(sf::FloatRect const& bounds) const;
-
-    std::vector<std::weak_ptr<Object>> getAllObjects() const;
-
-    /// Member variable declarations
-
-    sf::FloatRect m_border;
-    size_t const m_depth;
-    size_t const& m_maxDepth, m_maxObjects;
-
-    // Pointers
-    Node* m_head{ nullptr };
-    std::vector<std::weak_ptr<Object>> m_objects{ };
-    std::vector<std::weak_ptr<Object>> m_collidedObjects{ };
-
-    std::array<std::unique_ptr<Node>, 4> m_childNodes{ { nullptr, nullptr, nullptr, nullptr } };
+    size_t getIndex(sf::FloatRect t_objectBounds) const;
+    std::vector<std::weak_ptr<Object>> getColliders(sf::FloatRect const& t_objectBounds) const;
+    std::vector<std::weak_ptr<Object>> getObjects() const;
 
     bool m_canSubdivide{ false };
+    sf::FloatRect m_shapeBounds;
+    size_t const m_depth;
+    size_t const& m_maxDepth;
+    size_t const& m_maxObjects;
+    Node* m_head{ nullptr };
+    std::vector<std::weak_ptr<Object>> m_objects{ };
+    std::vector<std::weak_ptr<Object>> m_colliders{ };
+    std::array<std::unique_ptr<Node>, 4> m_childNodes{ { nullptr, nullptr, nullptr, nullptr } };
 };
